3dcbaaebcf2e23809f5a241c07079bafb763ba0e
Ishaan Kathiriya <ishaankathiriya2005@gmail.com>
2025-10-16 10:33:03 +0530

    Added gitignore

--- build/turtlebot3_bringup/colcon_command_prefix_build.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/ishaan/turtlebot_sim/install/turtlebot3_description/share/turtlebot3_description/package.sh"
-. "/home/ishaan/turtlebot_sim/install/turtlebot3_node/share/turtlebot3_node/package.sh"
--- build/turtlebot3_node/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ishaan/turtlebot_sim/turtlebot3/turtlebot3_node
-# Build directory: /home/ishaan/turtlebot_sim/build/turtlebot3_node
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
